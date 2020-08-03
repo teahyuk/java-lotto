a38542d501723b071b95b7faff9b8708b7ae2011
정태혁 <jtaeh57@gmail.com>
2020-08-03 11:39:52 +0900

    add : ui class static strings

---
+++ src/main/java/ui/InputView.java
@@ -0,0 +1,8 @@
+package ui;
+
+public class InputView {
+    private static final String PURCHASE_PRISE="구입금액을 입력해 주세요.";
+    private static final String PURCHASE_COUNT="수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String WIN_NUMBER="지난 주 당첨 번호를 입력해 주세요.";
+    private static final String WIN_BONUS_NUMBER="보너스 볼을 입력해 주세요.";
+}
---
+++ src/main/java/ui/OutputView.java
@@ -0,0 +1,10 @@
+package ui;
+
+public class OutputView {
+    private static final String BUY_TICKET_FORMAT="수동으로 %d장, 자동으로 %d개를 구매했습니다.";
+    private static final String CHECK_WIN="당첨 통계\n---------";
+    private static final String CHECK_WIN_FORMAT="%d개 일치%s (%d원)- %d개";
+    private static final String BONUS_STRING=", 보너스 볼 일치";
+    private static final String RESULT_FORMAT="총 수익률은 %.2f입니다.";
+    private static final String LOTTO_NUMBER_FORMAT="[%d,%d,%d,%d,%d,%d]";
+}
